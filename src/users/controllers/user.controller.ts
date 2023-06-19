072731ef16faf363b4360f5086565dc0027c2f03
eideard-hm <edierhernandezmo@gmail.com>
2023-06-19 12:07:59 -0500

    Separte responsabilities

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -2,8 +2,22 @@ import { prisma } from '../../prisma';
 import type { User } from '../types';
 
 export const createUser = async (user: User): Promise<User> => {
-  const newUser = await prisma.user.create({
-    data: user,
-  });
-  return newUser;
+  try {
+    const newUser = await prisma.user.create({
+      data: user,
+    });
+    return newUser;
+  } catch (error) {
+    console.error(error);
+    return user;
+  }
+};
+
+export const getAllUsers = async (): Promise<User[]> => {
+  try {
+    return await prisma.user.findMany();
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
--- src/users/routes/user.routes.ts
+++ src/users/routes/user.routes.ts
@@ -1,14 +1,13 @@
-import { type Request, type Response, Router } from 'express';
+import { Router, type Request, type Response } from 'express';
 
-import { prisma } from '../../prisma';
-import { createUser } from '../controllers';
+import { createUser, getAllUsers } from '../controllers';
 import type { User } from '../types';
 
 const router = Router();
 
 // get all users
 router.get('/', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
+  const users = await getAllUsers();
   res.json({ users });
 });
 
