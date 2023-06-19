517aa6bdd8744e6d883ed492725ff5d232296304
eideard-hm <edierhernandezmo@gmail.com>
2023-06-18 19:22:31 -0500

    deploy un vercel

--- src/auth/routes/auth.routes.ts
+++ src/auth/routes/auth.routes.ts
@@ -1,13 +1,21 @@
+import { PrismaClient } from '@prisma/client';
 import { type Request, type Response, Router } from 'express';
 
 import { ILoginData } from '../models';
 
 const router = Router();
+const prisma = new PrismaClient();
+
+// get all users
+router.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json({ users });
+});
 
 router.post('/login', (req: Request, res: Response) => {
-  const { email, password  } = req.body as ILoginData;
+  const { email, password } = req.body as ILoginData;
 
-  console.log({email, password});
+  console.log({ email, password });
   res.json({ login: true });
 });
 
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,5 @@ import { SERVER_PORT } from './config';
 app.listen(SERVER_PORT, () =>
   console.log(`Server is running on port: ${SERVER_PORT}`)
 );
+
+export default app;
