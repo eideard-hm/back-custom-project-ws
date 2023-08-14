e8e6aaa91e83634e0d8d148673342b9c80ddd381
DIGITALWARE/EdierM <EdierM@digitalware.com.co>
2023-08-14 18:14:22 -0500

    clean code

--- src/users/routes/user.routes.ts
+++ src/users/routes/user.routes.ts
@@ -1,15 +1,9 @@
 import { Router, type Request, type Response } from 'express';
 
-import { createUser, getAllUsers } from '../controllers';
+import { createUser } from '../controllers';
 import type { UserCreateInput } from '../types';
 
 const router = Router();
-
-// get all users
-router.get('/', async (req: Request, res: Response) => {
-  const users = await getAllUsers();
-  res.json({ users });
-});
 
 // create new user
 router.post('/', async (req: Request, res: Response) => {
